f746c701f088f846c1e6568e08eab1010ec18248
Ibrahim Tijani <144402297+teebabs521@users.noreply.github.com>
2024-02-18 11:22:30 +0100

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -2,5 +2,6 @@ export const aboutData = {
     title: "About Me",
     description1: "My name is Ibrahim Tunde. I'm a Freelance Web Designer and Frontend Developer from Nigeria.",
     description2: "A frontend developer passionate about blending design and code to create visually stunning and user-friendly websites. I also embrace the power of low-code/no-code tools, like Wordpress and Squarespace, to deliver high-quality projects with greater flexibility, all at a fraction of the usual cost.",
+    description3: "Elevator Pitch Link - https://diversitytech.com.ng/wp-content/uploads/2024/02/My-Elevator-Pitch.mp4
     image: 2
 }
